287bca1d07b8a824d5f7c30fa6fff2d30d005f4b
ChandanJana <chandanjana@gmail.com>
2018-04-16 14:07:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/janac/firebsedataoperation/CustomArtistAdapter.java
@@ -0,0 +1,37 @@
+package com.example.janac.firebsedataoperation;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomArtistAdapter extends ArrayAdapter<Artist> {
+    private Activity context;
+    List<Artist> artists;
+
+    public CustomArtistAdapter(Activity context, List<Artist> artists) {
+        super(context, R.layout.layout_artist_list, artists);
+        this.context = context;
+        this.artists = artists;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.layout_artist_list, null, true);
+
+        TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
+        TextView textViewGenre = (TextView) listViewItem.findViewById(R.id.textViewGenre);
+
+        Artist artist = artists.get(position);
+        textViewName.setText(artist.getArtistName());
+        textViewGenre.setText(artist.getArtistGenre());
+
+        return listViewItem;
+    }
+}
